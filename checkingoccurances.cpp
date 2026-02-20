67064e1ba2af42d220b491cfaf5fcb077d5e31e1
Akshat Rastogi <154761569+requiremen@users.noreply.github.com>
2026-02-21 00:32:48 +0530

    Create checkingoccurances.cpp

---
+++ checkingoccurances.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n,x;
+    cin>>n;
+    cin>>x;
+    int arr[n];
+    for(int i = 0; i<n; i++){
+        cin>>arr[i];
+    }
+    int count = 0;
+    for(int i = 0; i<n;i++){
+        if(arr[i]==x){
+            count++;
+    
+            }
+        
+
+    }
+   cout<<count;
+   
+   
+}
