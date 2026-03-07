4291fabffa500a9e56bd10f30a3ebd27740379a3
Akshat Rastogi <154761569+requiremen@users.noreply.github.com>
2026-03-08 01:52:10 +0530

    Create brothers.cpp

---
+++ brothers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string s1,s2;
+    cin>>s1>>s2;
+    string t1,t2;
+    cin>>t1>>t2;
+    if(s2==t2){
+        cout<<"YES";
+    }else{
+        cout<<"NO";
+    }
+}
+INPUT
+Ali Hassan
+Omar Hassan
