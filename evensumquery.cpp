618ef0cd421b51744ffe3dfc1f5985af5d7a61bc
Akshat Rastogi <154761569+requiremen@users.noreply.github.com>
2026-04-19 12:37:58 +0530

    even sum query program in evensumquery.cpp
    Implement a program to calculate the sum of even numbers in a range based on user queries.

---
+++ evensumquery.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    int a[n];
+    for(int i = 0;i<n;i++){
+        cin>>a[i];
+    }
+
+    int p[n];
+    int sum = 0;
+
+    for(int i = 0;i<n;i++){
+        if(a[i]%2==0){
+            sum += a[i];
+        }
+        p[i] = sum;
+    }
+
+    int q;
+    cin >> q;  
+
+    for(int i = 0;i<q;i++){
+        int l,r;
+        cin>>l>>r;
+
+        l--;
+        r--;
+
+        if(l==0){
+            cout<<p[r]<<endl;
+        }else{
+            cout<<p[r]-p[l-1]<<endl;
+        }
+    }
+}
